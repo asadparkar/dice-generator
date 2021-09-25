14654d224bfa5d8017991f9b942ab34806bc8a19
asadparkar <58441390+asadparkar@users.noreply.github.com>
2021-09-25 23:03:20 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+var image1= document.querySelectorAll("img")[0];
+var randomimage1= "images/dice"+randomNumber1+".png";
+image1.setAttribute("src",randomimage1);
+
+
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+var image2= document.querySelectorAll("img")[1];
+var randomimage2= "images/dice"+randomNumber2+".png";
+image2.setAttribute("src",randomimage2);
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML="Player 1 wins!";
+} else if (randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerHTML="Player 2 wins!";
+} else{
+    document.querySelector("h1").innerHTML="Thats a tie!";
+}
